f9a41a31c5fe96c0001e9e4f7ab2059daea8ca6c
Donatas <adamonis@mail.com>
2020-08-26 11:55:54 +0300

    Beneficiary builder

--- src/main/java/addendum/challenge/model/Beneficiary.java
+++ src/main/java/addendum/challenge/model/Beneficiary.java
@@ -1,29 +1,53 @@
 package addendum.challenge.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 import java.util.UUID;
 
+@JsonDeserialize(builder = Beneficiary.BeneficiaryBuilder.class)
 public class Beneficiary {
-    private final Integer id;
-    private final UUID unique_code;
-    private final String name;
+    private Integer id;
+    private UUID uniqueCode;
+    private String name;
 
-    public Beneficiary(
-            Integer id,
-            UUID unique_code,
-            @JsonProperty("name") String name
-    ) {
-        this.id = id;
-        this.unique_code = unique_code;
-        this.name = name;
+    public static class BeneficiaryBuilder {
+        private Integer id;
+        private UUID uniqueCode;
+        private String name;
+
+        @JsonProperty("id")
+        public BeneficiaryBuilder id(Integer id){
+            this.id = id;
+            return this;
+        }
+        @JsonProperty("uniqueCode")
+        public BeneficiaryBuilder uniqueCode(UUID uniqueCode){
+            this.uniqueCode = uniqueCode;
+            return this;
+        }
+        @JsonProperty("name")
+        public BeneficiaryBuilder name(String name){
+            this.name = name;
+            return this;
+        }
+        public Beneficiary build() {
+            return new Beneficiary(this);
+        }
+
+    }
+    private Beneficiary(BeneficiaryBuilder builder) {
+        this.id = builder.id;
+        this.uniqueCode = builder.uniqueCode;
+        this.name = builder.name;
     }
 
     public Integer getId() {
         return id;
     }
 
-    public UUID getUnique_code() {
-        return unique_code;
+    public UUID getUniqueCode() {
+        return uniqueCode;
     }
 
     public String getName() {
@@ -31,6 +55,6 @@ public class Beneficiary {
     }
 
     public String toJson(){
-        return "{\"name\":" + this.name + "\",\"unique_code\":\"" + this.unique_code + "\"}";
+        return "{\"name\":" + this.name + "\",\"unique_code\":\"" + this.uniqueCode + "\"}";
     }
 }
